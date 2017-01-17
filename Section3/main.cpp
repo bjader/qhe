d5c476df254756a8f54f28eafa36aa6f0e046be7
Ben Jaderberg <benjamin.jaderberg.13@ucl.ac.uk>
2017-01-17 16:57:32 +0000

    Initial Commit

---
+++ Section3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Section3
+//
+//  Created by Benjamin Jaderberg on 17/01/2017.
+//  Copyright © 2017 BenJad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
